5ec27129a72aa9cf8ede5a79895c9dc634492546
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-05-05 20:26:47 +0530

    ADDED: Inform user if entered invalid mood

---
+++ moodAnalysisException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    class moodAnalysisException : Exception
+    {
+        public enum ExceptionType
+        {
+            EMPTY, NULL
+        }
+        ExceptionType type;
+        public moodAnalysisException(ExceptionType type, string massage) : base(massage)
+        {
+            this.type = type;
+        }
+    }
+}
